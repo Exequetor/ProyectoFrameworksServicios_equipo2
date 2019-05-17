75580588ab78e08b2003443d24a3b6d29c805aff
Lab-ssistemas <Lab-ssistemas@192.168.244.118>
2019-05-17 08:14:21 -0600

    Test de Marcas y Productos

---
+++ src/test/java/cursoNTecnologias/service/marcas/MarcasServiceImplTest.java
@@ -0,0 +1,60 @@
+package cursoNTecnologias.service.marcas;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cursoNTecnologias.bd.domain.Marcas;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/applicationContext.xml"})
+public class MarcasServiceImplTest {
+	
+	@Inject 
+	MarcasServiceImpl marcaService;
+	
+	
+	public void pruebaMostrarMarcas(){
+		try{
+			List<Marcas> lista= marcaService.listarMarcas();
+		}catch(Exception  e){
+			
+		}
+	}
+	
+	public void agregaMarca(){
+		try{
+			Marcas marca = new Marcas();
+			marca.setIdmarca(0);
+			marca.setNombremarca("Epson");
+			
+			marcaService.insertMarca(marca);
+		}
+		catch (Exception ex) {
+			System.out.println("Error: " + ex);
+		}
+	}
+	@Test
+	public void deleteMarca() {
+		try{
+			marcaService.deleteMarca(4);	
+		}
+		catch (Exception ex) {
+			System.out.println("Error: " + ex);
+		}
+	}
+	
+	public void updateMarca(){
+		Marcas marca = new Marcas();
+		marca.setIdmarca(2);
+		marca.setNombremarca("Realteck");
+		marcaService.updateMarca(marca);
+	}
+}
